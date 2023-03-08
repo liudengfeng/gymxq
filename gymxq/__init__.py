80b46956c40a872e6dea4ab1e10109dd0c20e934
LDF <liudengfeng_sd@outlook.com>
2023-03-08 13:56:22 +0800

    300 -> 200

--- gymxq/__init__.py
+++ gymxq/__init__.py
@@ -3,7 +3,7 @@ from gymnasium.envs.registration import register
 register(
     id="xqv0",
     entry_point="gymxq.envs:XiangQiV0",
-    max_episode_steps=300,
+    max_episode_steps=200,
     # order_enforce=False,
     # disable_env_checker=True,
 )
@@ -11,7 +11,7 @@ register(
 register(
     id="xqv1",
     entry_point="gymxq.envs:XiangQiV1",
-    max_episode_steps=300,
+    max_episode_steps=200,
     # order_enforce=False,
     # disable_env_checker=True,
 )
